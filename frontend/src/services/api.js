24d06ff2b67f9b3a211218096768d13be98f7d5b
rebaka-45427 <frebakajesi@gmail.com>
2026-07-04 22:51:34 +0530

    Updated API URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: "https://campus-pilot.onrender.com",
 });
 
 // Request interceptor to add token
